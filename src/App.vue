6fa49dc2e1b33b46bc24cbe215ff5e0ea3829ee3
Brycen Harmon <brycenharmon@gmail.com>
2022-05-07 12:52:19 -0500

    created toggleReminder method, emitted it to higher level App.vue, and added functionality through map()

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
@@ -22,7 +22,12 @@ export default {
   },
   methods: {
     deleteTask(id) {
-      console.log('task',id)
+      if (confirm('Are you sure you want to delete this task?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task ) //map through tasks, if the id matched the id clicked, reverse reminder truthy value, if not, return task
     }
   },
   created() {
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,5 @@
 <template>
-<div :class="[task.reminder ? 'reminder' : '', 'task']" > <!--the class is task, but also, if the task.reminder is true, the class is also "reminder"-->
+<div @dblclick="onReminder(task.id)" :class="[task.reminder ? 'reminder' : '', 'task']" > <!--the class is task, but also, if the task.reminder is true, the class is also "reminder"-->
     <h3>{{task.text}}
     <i @click="onDelete(task.id)" class="fas fa-times"></i></h3>
     <p>{{task.day}}</p>
@@ -15,6 +15,9 @@
         methods: {
             onDelete(id) {
                 this.$emit('delete-task', id)
+            },
+            onReminder(id) {
+                this.$emit('toggle-reminder', id)
             }
         }
     }
